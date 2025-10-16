2d1c03302ab204cdff62923a1e205f2922eef9cb
Michael Chernyakoff <chernyakoff@gmail.com>
2025-10-16 04:01:58 +0300

    update config.yml, generate-env.js, header.vue and global.d.ts

--- web/scripts/generate-env.js
+++ web/scripts/generate-env.js
@@ -24,6 +24,9 @@ let envContent = ''
 if (config.api?.url) {
   envContent += `API_URL=${config.api.url}\n`
 }
+if (config.api?.bot?.name) {
+  envContent += `BOT_NAME=${config.api.bot.name}\n`
+}
 
 fs.writeFileSync(envFile, envContent)
 console.log(envContent)
