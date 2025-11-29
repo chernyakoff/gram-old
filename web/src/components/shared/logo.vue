0d46a717758714c7a7a76eb5a21226659c910a7b
Michael Chernyakoff <chernyakoff@gmail.com>
2025-11-29 16:01:34 +0300

    fix

--- web/src/components/shared/logo.vue
+++ web/src/components/shared/logo.vue
@@ -1,12 +1,11 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div class="flex items-center space-x-2">
-    <img src="@/assets/logo.svg" alt="Neurogram" class="h-8" />
-    <span v-if="!collapsed" class="font-bold text-lg">Neurogram</span>
+    <img src="@/assets/logo.svg" alt="Lidorub" class="h-8" />
+    <span v-if="!collapsed" class="font-bold text-lg">Lidorub</span>
   </div>
 </template>
 <script setup lang="ts">
-
 defineProps<{
   collapsed?: boolean
 }>()
