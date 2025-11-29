639ae0864a57b5c0475c6fea1b4e16f93be80b91
Michael Chernyakoff <chernyakoff@gmail.com>
2025-11-29 16:07:47 +0300

    fix

--- web/src/components/shared/logo.vue
+++ web/src/components/shared/logo.vue
@@ -1,10 +1,16 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div class="flex items-center space-x-2">
-    <img src="@/assets/logo.svg" alt="Lidorub" class="h-8" />
+    <img
+      src="@/assets/logo.svg"
+      alt="Lidorub"
+      class="h-8 invert-0 dark:invert brightness-0 dark:brightness-100"
+    />
+
     <span v-if="!collapsed" class="font-bold text-lg">Lidorub</span>
   </div>
 </template>
+
 <script setup lang="ts">
 defineProps<{
   collapsed?: boolean
