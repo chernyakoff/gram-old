a5de8a0dc00f07b6a0345fb57dc35748f8a39d1f
Michael Chernyakoff <chernyakoff@gmail.com>
2025-12-12 22:35:41 +0300

    fix

---
+++ api/app/cli/aimodel.py
@@ -0,0 +1,35 @@
+from typing import Any, cast
+
+from cyclopts import App
+from openai import AsyncOpenAI
+from rich import print
+
+from app.app import app as api
+from app.config import config
+
+app = App(name="aimodel", help="Generate OpenApi spec")
+
+
+params = {
+    "api_key": config.openai.api_key,
+    "timeout": config.openai.timeout,
+}
+if config.openai.base_url:
+    params["base_url"] = config.openai.base_url
+
+
+client = AsyncOpenAI(**params)
+
+
+@app.default
+async def _(model: str):
+    messages = [{"role": "user", "content": "Привет"}]
+    try:
+        raw_response = await client.responses.create(
+            model=model,  # например "gpt-4.1" или "gpt-3.5-turbo"
+            input=cast(Any, messages),
+        )
+
+        print(raw_response.output_text)  # completion.choices[0].message.content or ""
+    except Exception as e:
+        print(e)
