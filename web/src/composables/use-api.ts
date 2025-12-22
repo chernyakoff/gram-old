e3e294fce2dad9cc1f1f0b7e397686eefeeeff13
Michael Chernyakoff <chernyakoff@gmail.com>
2025-12-22 20:16:05 +0300

    fix

--- web/src/composables/use-api.ts
+++ web/src/composables/use-api.ts
@@ -10,61 +10,14 @@ export function getErrorValue(e: unknown): string {
 }
 
 // ---------- Ky instance с Authorization + snake/camel + refresh ----------
-/* function createKy() {
-  const authStore = useAuthStore()
 
-  return ky.create({
-    prefixUrl: import.meta.env.API_URL,
-    credentials: 'include',
-    hooks: {
-      beforeRequest: [
-        async (request) => {
-          if (authStore.accessToken)
-            request.headers.set('Authorization', `Bearer ${authStore.accessToken}`)
-        },
-      ],
-      afterResponse: [
-        async (request, options, response) => {
-          if (response.status === 401) {
-            options.headers = options.headers || new Headers()
-            const headers =
-              options.headers instanceof Headers ? options.headers : new Headers(options.headers)
-
-            if (!headers.get('_retry')) {
-              headers.set('_retry', 'true')
-
-              const authStore = useAuthStore()
-              const success = await authStore.refreshTokens()
-
-              if (success) {
-                headers.set('Authorization', `Bearer ${authStore.accessToken}`)
-                options.headers = headers
-
-                // ВАЖНО: передаём не request.url, а сам request
-                return ky(request, options)
-              }
-            }
-          }
-
-          // camelCase
-          if (response.ok && response.headers.get('content-type')?.includes('application/json')) {
-            const data = await response.json()
-            return new Response(JSON.stringify(keysToCamel(data)), response)
-          }
-
-          return response
-        },
-      ],
-    },
-  })
-} */
 function createKy() {
   const authStore = useAuthStore()
 
   return ky.create({
     prefixUrl: import.meta.env.API_URL,
     credentials: 'include',
-    timeout: 30000,
+    timeout: 5 * 60 * 1000,
     hooks: {
       beforeRequest: [
         async (request) => {
@@ -108,31 +61,6 @@ export function useApi() {
   const error = ref<string | null>(null)
   const success = ref(false)
   const kyInstance = createKy()
-
-  /* const api = async <T>(endpoint: string, options?: AnyOptions): Promise<T> => {
-    loading.value = true
-    error.value = null
-    success.value = false
-    //endpoint = normalizeEndpoint(endpoint)
-    try {
-      const opts = { ...options }
-
-      if (opts.body && !(opts.body instanceof FormData) && typeof opts.body !== 'string') {
-        opts.body = JSON.stringify(keysToSnake(opts.body))
-        opts.headers = { ...opts.headers, 'Content-Type': 'application/json' }
-      }
-
-      const response = await kyInstance(endpoint, opts as Options)
-      const data = await response.json()
-      success.value = true
-      return data as T
-    } catch (e: unknown) {
-      error.value = getErrorValue(e)
-      throw e
-    } finally {
-      loading.value = false
-    }
-  } */
 
   const api = async <T>(endpoint: string, options?: AnyOptions): Promise<T> => {
     loading.value = true
